9c6349a08842811c2ce254738b737398ae212915
malenagrannerud <126866109+malenagrannerud@users.noreply.github.com>
2025-10-30 12:27:43 +0100

    test

--- src/data/tips.ts
+++ src/data/tips.ts
@@ -13,7 +13,7 @@ export const tips: Tip[] = [
   {
     id: 1,
     title: "Fem nävar frukt och grönt",
-    color: "bg-green-300",
+    color: "bg-green-100",
     textColor: "text-blue-900",
     healthScore: 4,
     description: "Frukt",
