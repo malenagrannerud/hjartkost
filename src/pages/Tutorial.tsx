38d27d57e5766499230df274cf2e05164452aea4
malenagrannerud <126866109+malenagrannerud@users.noreply.github.com>
2025-10-31 14:04:25 +0100

    0

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -23,7 +23,7 @@ const Tutorial = () => {
           </button>
           {/* STANDARDIZATION: Title uses heading, subtitle uses pageSubeading(text-lg) */}
           <h1 className={sectionHeading}>Så fungerar appen</h1>
-          <p className={`${sectionSubheading} mt-3`}>En guide för dig</p>
+          <p className={sectionSubheading}>En guide för dig</p>
         </div>
       </div>
 
